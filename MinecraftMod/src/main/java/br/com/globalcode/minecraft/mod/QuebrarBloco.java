3cb5d7eafa7f13e5a458c3e3ba3cc3018f381095
Global Code <Global Code@senger-win.linux>
2017-07-15 15:07:35 -0300

    IoT integration

--- MinecraftMod/src/main/java/br/com/globalcode/minecraft/mod/QuebrarBloco.java
+++ MinecraftMod/src/main/java/br/com/globalcode/minecraft/mod/QuebrarBloco.java
@@ -1,21 +1,35 @@
 package br.com.globalcode.minecraft.mod;
+
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import br.com.globalcode.basico.Computador;
+import br.com.globalcode.minecraft.GlobalcodeMod;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.eclipse.paho.client.mqttv3.MqttException;
 
 public class QuebrarBloco {
-	String seuNome = "Vinicius";
-	
+
+    String seuNome = "Vinicius";
+    static int relayStatus = 0;
+
     @SubscribeEvent
-    public void sendMessage(BreakEvent event){
+    public void sendMessage(BreakEvent event) {
         event.
-            getPlayer()
-            .addChatMessage(
-                new ChatComponentText(
-                    EnumChatFormatting.GOLD +
-                seuNome + " quebrou um bloco!"));
+                getPlayer()
+                .addChatMessage(
+                        new ChatComponentText(
+                                EnumChatFormatting.GOLD
+                                + seuNome + " quebrou um bloco!"));
         Computador.falar(seuNome + " quebrou um bloco!", true);
+        try {
+            GlobalcodeMod.client.publish("/iot-surfboard/control", ("relay?" + relayStatus).getBytes(), 0, false);
+        } catch (MqttException ex) {
+            Logger.getLogger(QuebrarBloco.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        relayStatus = relayStatus == 1 ? 0 : 1;
+
     }
 }
